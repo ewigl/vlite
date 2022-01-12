8de8a89793946bbc8e7d06c1bb4880b71288346c
ewigl <ewiglichtl@163.com>
2022-01-12 22:30:08 +0800

    update

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import HelloWorld from '../components/HelloWorld.vue'
 import Layout from '@layout/index.vue'
 import Dashboard from '@views/Dashboard/index.vue'
 
 
 const routes = [
     { path: '/', component: Layout },
-    { path: '/hello', component: HelloWorld },
     { path: '/dashboard', component: Dashboard },
 ]
 
