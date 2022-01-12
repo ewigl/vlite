04a28e63b5c86121369913887df32275f8eebffe
ewigl <ewiglichtl@163.com>
2022-01-12 16:19:05 +0800

    update tsconfig

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,20 @@
 import { InjectionKey } from 'vue'
 import { createStore, useStore as baseUseStore, Store } from 'vuex'
 
-
 export interface State {
-    count: number
+  count: number
+  name: string
 }
 
 export const key: InjectionKey<Store<State>> = Symbol()
 
 export const store = createStore<State>({
-    state: {
-        count: 0
-    }
+  state: {
+    count: 0,
+    name: 'Vue'
+  }
 })
 
 export function useStore() {
-    return baseUseStore(key)
+  return baseUseStore(key)
 }
