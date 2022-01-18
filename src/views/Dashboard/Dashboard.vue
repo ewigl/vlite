7adb816a0ac8233c54cea0bf3dc2f993741adde3
ewigl <ewiglichtl@163.com>
2022-01-18 13:31:32 +0800

    temp

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,8 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Template from './components/Template.vue'
+</script>
 
 <template>
-  <div>Dashboard</div>
-  <el-button :disabled="true" type="text" size="default">textbutton</el-button>
+  <div>
+    <div>Dashboard</div>
+    <el-button :disabled="true" type="text" size="default">
+      textbutton
+    </el-button>
+    <Template msg="Hello L"></Template>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/views/Dashboard/components/Template.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// import
+defineProps<{
+  msg: string
+  name?: string
+}>()
+</script>
+
+<template>
+  {{ msg }}
+</template>
