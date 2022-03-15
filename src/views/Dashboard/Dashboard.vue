c79f29fef492a9c9a15a3ddf59857e14c5e64aa4
ewigl <ewiglichtl@163.com>
2022-03-15 15:58:32 +0800

    style fix

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import Chart from '@/components/Chart/Chart.vue'
+
 let lineChartData = {
   xAxis: {
     type: 'category',
@@ -17,41 +18,6 @@ let lineChartData = {
   ]
 }
 
-let pieChartData = {
-  title: {
-    text: 'Referer of a Website',
-    subtext: 'Fake Data',
-    left: 'center'
-  },
-  tooltip: {
-    trigger: 'item'
-  },
-  legend: {
-    orient: 'vertical',
-    left: 'left'
-  },
-  series: [
-    {
-      name: 'Access From',
-      type: 'pie',
-      radius: '50%',
-      data: [
-        { value: 1048, name: 'Search Engine' },
-        { value: 735, name: 'Direct' },
-        { value: 580, name: 'Email' },
-        { value: 484, name: 'Union Ads' },
-        { value: 300, name: 'Video Ads' }
-      ],
-      emphasis: {
-        itemStyle: {
-          shadowBlur: 10,
-          shadowOffsetX: 0,
-          shadowColor: 'rgba(0, 0, 0, 0.5)'
-        }
-      }
-    }
-  ]
-}
 onMounted(() => {
   // console.log('onMounted Dashboard')
   // console.log('MODE:' + import.meta.env.MODE)
@@ -66,9 +32,6 @@ onMounted(() => {
     </el-col>
     <el-col :span="12">
       <Chart class="e-charts" :options="lineChartData"></Chart>
-    </el-col>
-    <el-col :span="12">
-      <Chart class="e-charts" :options="pieChartData"></Chart>
     </el-col>
   </el-row>
 </template>
