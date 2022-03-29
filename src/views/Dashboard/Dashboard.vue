ec4fb3af6106a05d4835d48041b5560fa7ad6a3e
ewigl <ewiglichtl@163.com>
2022-03-29 17:30:00 +0800

    weather

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { onMounted, reactive, ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { getCityList, getCurrentWeather } from '@/api/dashboard'
 import debounce from 'lodash/debounce'
+import weatherKeyMaps from './components/weatherKeyMaps'
+import type { WeatherInfo } from './types'
 
 const currentCity = ref(JSON.parse(localStorage.getItem('currentCity') || '{}'))
 const selectedCity = ref<string>('')
 const selectLoading = ref(false)
 const cities = ref<ListItem[]>([])
-const currentWeather = ref({
-  weather: []
-})
+const currentWeather = ref<WeatherInfo>()
 
 onMounted(() => {
   getCurrentWeather({
@@ -18,10 +18,14 @@ onMounted(() => {
     appid: import.meta.env.V_WEATHER_APPID,
     units: 'metric',
     lang: 'zh_cn'
-  }).then((res) => {
-    // console.log(res.data)
-    currentWeather.value = res.data
   })
+    .then((res) => {
+      console.log('currentWeather', res.data)
+      currentWeather.value = res.data
+    })
+    .catch((err) => {
+      console.log('currentWeather err', err.response.data.message)
+    })
 })
 
 interface ListItem {
@@ -82,10 +86,14 @@ const setCity = (value: object) => {
           />
         </el-select>
         <span style="margin-left: 1rem"> 当前城市: {{ currentCity.name }}</span>
-        <div>
-          {{ currentWeather }}
-          <!-- <img src="https://i.i8tq.com/e_index/todayweather/00_d.png" alt="" /> -->
+        <div v-if="currentWeather?.weather">
+          <svg-icon
+            class="weather-icon"
+            :name="weatherKeyMaps.get(currentWeather?.weather[0].icon)"
+          ></svg-icon>
+          {{ currentWeather?.weather[0].description }}
         </div>
+        <div v-else>加载中...</div>
       </el-card>
     </el-col>
   </el-row>
@@ -98,5 +106,9 @@ const setCity = (value: object) => {
 }
 .weather-board {
   height: 24rem;
+}
+.weather-icon {
+  width: 4rem;
+  height: 4rem;
 }
 </style>
---
+++ src/views/Dashboard/types.ts
@@ -0,0 +1,10 @@
+type Weather = {
+  description: string
+  icon: string
+}
+
+export type WeatherInfo = {
+  description: string
+  icon: string
+  weather: Weather[]
+}
